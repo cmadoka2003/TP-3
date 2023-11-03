93a124f5c071b5587650cbdaed0908aeec3523c6
Florent <florent.grondin2@ent.univ-lorraine.fr>
2023-11-03 16:08:15 +0100

    fix bug for animation and top destination image

--- src/js/script.js
+++ src/js/script.js
@@ -17,67 +17,69 @@ function resetImages(element) {
   allTextTr.forEach(function (text) {
     text.style.filter = "none"; // Mettre en flou les autres images
   });
-  var texteSurvol = document.querySelector(".texte-survol");
-  if (texteSurvol) {
-    texteSurvol.remove();
-  }
+  
 }
 
 function redirectToPage(page) {
   window.location.href = page; // Redirection vers la page spécifiée
 }
 
-function enlargeImage(element) {
-  element.style.transform = "scale(1.1)"; // Agrandir l'image
+function enlargeImage(elementP) {
+  elementP.style.transform = "scale(1.1)"; // Agrandir l'image
+  let element=elementP.childNodes[1].childNodes[1]
   let allDiv = document.querySelectorAll(".image-container");
   let allImages = document.querySelectorAll(".custom-image");
+
   let allTextBL = document.querySelectorAll(".text-bottom-left");
   let allTextBr = document.querySelectorAll(".text-bottom-right");
   let allTextTr = document.querySelectorAll(".text-top-left");
   allImages.forEach(function (img) {
-    if (img !== element.childNodes[1].childNodes[1]) {
+    if (img !== element.childNodes[1]) {
       img.style.filter = "blur(4px)"; // Mettre en flou les autres images
     }
   });
   allTextBr.forEach(function (text) {
-    if (text !== element.childNodes[1].childNodes[9]) {
+    if (text !== element.childNodes[9]) {
       text.style.filter = "blur(4px)"; // Mettre en flou les autres images
     }
   });
   allTextBL.forEach(function (text) {
     if (
-      text !== element.childNodes[1].childNodes[5] &&
-      text !== element.childNodes[1].childNodes[7]
+      text !== element.childNodes[5]&&
+      text !== element.childNodes[7]
     ) {
       text.style.filter = "blur(4px)"; // Mettre en flou les autres images
     }
   });
   allTextTr.forEach(function (text) {
-    if (text !== element.childNodes[1].childNodes[3]) {
+    if (text !== element.childNodes[3]) {
       text.style.filter = "blur(4px)"; // Mettre en flou les autres images
     }
   });
-  let texteSurvol = null;
+  let texteSurvol = element.querySelector(".texte-survol");
 
-  if (element.href.includes("#")) {
+  if (!texteSurvol) {
     texteSurvol = document.createElement("div");
-    texteSurvol.textContent = "Pas d'information pour l'instant";
+
+    if (elementP.childNodes[1].href.includes("#")) {
+      texteSurvol.textContent = "Pas d'information pour l'instant";
+    } else {
+      texteSurvol.textContent = "Cliquez pour voir plus";
+    }
+
     texteSurvol.className = "texte-survol";
-  } else {
-    texteSurvol = document.createElement("div");
-    texteSurvol.textContent = "Cliquez pour voir plus";
-    texteSurvol.className = "texte-survol";
+    element.appendChild(texteSurvol);
   }
 
-  texteSurvol.addEventListener("mouseover", function (event) {
-    event.stopPropagation(); // Empêche la propagation de l'événement de souris
+  elementP.addEventListener("mouseout", function () {
+    texteSurvol.style.display = "none"; // Masquer l'élément texteSurvol lorsque la souris quitte l'élément
   });
 
-  element.appendChild(texteSurvol);
-  texteSurvol.addEventListener("mouseout", function () {
-
-    texteSurvol.remove(); // Retirer l'élément texteSurvol lorsque la souris quitte l'élément
+  elementP.addEventListener("mouseover", function () {
+    texteSurvol.style.display = "block"; // Réafficher l'élément texteSurvol lorsque la souris rentre dans l'élément
   });
+
+ 
 }
 function changerImage(src) {
   var imageSelectionnee = document.getElementById("imageSelectionnee");
@@ -87,5 +89,7 @@ function changerImage(src) {
 document.addEventListener("DOMContentLoaded", function () {
   let today = new Date().toISOString().split("T")[0];
   document.getElementById("date").value = today;
+ 
+
 });
 
